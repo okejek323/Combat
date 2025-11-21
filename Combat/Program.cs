48d07f5343f285816ef9826a7433f4a693024220
okejek323 <144665821+okejek323@users.noreply.github.com>
2025-11-21 15:01:06 +0100

    v0.1

--- Combat/Program.cs
+++ Combat/Program.cs
@@ -1,14 +1,20 @@
-﻿using Combat;
+using Combat;
 bool doCzasu = true;
 bool gra = true;
 
-Gracz player = new Gracz();
-Zombie zombie = new Zombie();
+Random rnd = new Random();
 
-Console.WriteLine("Podaj swoją nazwe: ");
+Gracz player = new Gracz();
+
+List<Potwor> Potwory = new List<Potwor>();
+
+Potwory.Add(new Zombie());
+
+int PotworyLen = Potwory.Count;
 
 while (doCzasu) {
-    string imie = Console.ReadLine();
+	Console.WriteLine("Podaj swoją nazwe: ");
+	string imie = Console.ReadLine();
     if (imie is not null)
     {
         player.Nazwa = imie;
@@ -18,17 +24,74 @@ while (doCzasu) {
 
 while (gra)
 {
-    Console.WriteLine($"Witaj {player.Nazwa}. Wybierz jedną z opcji");
+    Console.Clear();
+    Console.WriteLine($"Witaj {player.Nazwa}");
 
     Console.WriteLine("Wybierz jedną z opcji: ");
-    Console.WriteLine("1 - Walka");
-    Console.WriteLine("2 - Ulepszenia");
-    Console.WriteLine("-------------");
+    Console.WriteLine("1 - Walka -- częściowo dostępne");
+    Console.WriteLine("2 - Ulepszenia -- WORK IN PROGRESS");
+    Console.WriteLine("3 - Potwory -- WORK IN PROGRESS");
     string wybór = Console.ReadLine();
 
     if (wybór == "1")
     {
-        Console.WriteLine("Walka się rozpoczyna!\n");
-    }
+        Console.WriteLine("Walka się rozpoczyna!");
+
+	    int FightWho = rnd.Next(1, PotworyLen);
+        switch (FightWho) 
+        { 
+            case 1:
+                Zombie zombie = new Zombie();
+                bool CzyPokonany = true;
+                Console.Clear();
+                Console.WriteLine($"Twój przeciwnik to: {zombie.Nazwa}");
+                while (CzyPokonany)
+                {
+                    Console.WriteLine("Wybierz jedną z akcji: ");
+                    Console.WriteLine("1 - Atakuj");
+                    Console.WriteLine("2 - Ulecz");
+
+                    int akcja = int.Parse(Console.ReadLine());
+                    switch (akcja)
+                    {
+                        case 1:
+							Console.Clear();
+							Console.WriteLine("Atakujesz!");
+                            zombie.Otrzymaj(player.Sila);
+                            Console.WriteLine($"Przeciwnik ma teraz {zombie.HP} HP");
+
+                            Console.ReadLine();
+
+                            Console.Clear();
+							Console.WriteLine("Przeciwnik atakuje!");
+							player.Otrzymaj(zombie.Sila);
+							Console.WriteLine($"Masz teraz {player.HP} HP");
+
+							Console.ReadLine();
+							Console.Clear();
+
+							if (zombie.HP == 0)
+                            {
+								Console.Clear();
+                                Console.WriteLine("Pokonałeś przeciwnika. Naciśnij by kontynuować.");
+                                Console.ReadLine();
+                                CzyPokonany = false;
+                            }
+                            if (player.HP == 0) 
+                            { 
+                                Console.Clear();
+                                Console.WriteLine("PORAZKA!");
+                                Console.WriteLine("Naciśnij dowolny przycisk by wyjść z gry...");
+								Console.ReadLine();
+                                gra = false;
+                                CzyPokonany= false;
+                            }
+							break;
+                    }
+                }
+                break;
+        }
+
+	}
 
 }
